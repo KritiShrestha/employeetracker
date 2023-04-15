cd7b0515f150e49f4cb023ac07d9dfb82365a614
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-04-15 14:21:22 -0400

    erased error

--- db/schema.sql
+++ db/schema.sql
@@ -4,18 +4,16 @@ CREATE DATABASE MyCompany_db;
 USE MyCompany_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
   CREATE TABLE employee (
@@ -24,8 +22,7 @@ CREATE TABLE role (
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NOT NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
