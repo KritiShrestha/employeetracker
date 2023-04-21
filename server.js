2eb05e12af4bb2b6ad5d8e9e71fbca7975233578
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-04-20 23:11:55 -0400

    Made changes

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 //Imports inquirer and mysql
 const inquirer = require('inquirer');
-const { default: Choice } = require('inquirer/lib/objects/choice');
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const questions = [
     {
         type: 'list',
         message: 'What would you like to do?',
         name: 'Choice',
-        choices: ['View all department', 'View all roles', 'View all employees', 'Add role', 'Add employee', 'Add department', 'Update an employee role']
+        choices: ['View all department', 'View all role', 'View all employee', 'Add role', 'Add employee', 'Add department', 'Update an employee role']
     }
-]
-       
+]      
 inquirer.prompt(questions).then((data) => {
   const db = mysql.createConnection({
     host: 'localhost',
@@ -23,14 +21,13 @@ inquirer.prompt(questions).then((data) => {
     db.query('SELECT * FROM department', function (err, results) {
       if (err) throw err;
       console.table(results);
-      db.end();
     });
-  } else if (data.Choice === 'View all roles') {
+  } else if (data.Choice === 'View all role') {
     db.query('SELECT * FROM role', function (err, results) {
       if (err) throw err;
       console.table(results);
     });
-  } else if (data.Choice === 'View all employees') {
+  } else if (data.Choice === 'View all employee') {
     db.query('SELECT * FROM employee', function (err, results) {
       if (err) throw err;
       console.table(results);
@@ -63,29 +60,83 @@ inquirer.prompt(questions).then((data) => {
 
       {
         type: 'input',
-        message: 'What is thesalary of the role?',
+        message: 'What is the salary for this role?',
         name:'Salary'
       },
 
       {
         type: 'input',
       message: 'Which department does the role belong to ?',
-      name: 'departmentChoice'
+      name: 'departmentChoice',
+      choice: ["Enginnering", "Finance", "Legal", "Sales"]
+      
       }
       ]
     ).then((answer) =>{
-      db.query('INSERT INTO role (title, salary, department_id, ) VALUES (? ? ?)'), [answer.roleName, answer.Salary, answer.departmentChoice], function (err, results) {
+      db.query('INSERT INTO role (title, salary, department_id) VALUES (? ? ?)'), [answer.roleName, answer.Salary, answer.departmentChoice], function (err, results) {
         if (err) throw err;
         console.log (`Added '${answer.roleName}' to the database`);
       }
     }
     ) 
    }
+   else if (data.name === 'Add an employee') {
+    inquirer.prompt ([
+      {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: "employeeFirstname"
+      },
+      {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: "employeeLastname"
+      },
+      {
+        type: 'input',
+        message: " What is the role of employee?",
+        name: "employeeRole",
+       choice:['Sales lead', 'Sales Person', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer']
+      },
+      {
+        type: 'input',
+        message: " Who is the employee of this manager?",
+        name: "employeeManager",
+        choice: ["None", "Ashley Smith", "Jane Johnson", "Sarah Brown", "Harry Badgley", "Rock Johnson", "Sam Payne", "Linda Burt", "Tom Lowel"]
+      
+      }
+    ]
+
+    ).then((anwser) => {
+      db.query('INSERT INTO employee(first_name, last_name,role_id, manager_id) VALUES (? ? ? ?)'), [answer.employeeFirstname, answer.employeeLastname, answer.employeeRole, answer.employeeManager], function (err, results){
+        if (err) throw err;
+        console.log(`Added "${employeeFirstname + employeeLastname} added to the role`)
+      }
+    }
+      )
+   }
+   else if (data.choice = "Update an employee role"){
+    inquirer.prompt(
+      [
+        {
+          type: 'input',
+          message: "Which employee's role do you want to update?",
+          name: updateEmployee
+        },
+        {
+          type: 'input',
+          message: "Which  role do you want to assign to this employee?",
+          choice: ['Sales lead', 'Sales Person', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer'],
+          name: employeeNewRole
+        }
+      ]
+    ).then((answer) => {
+      db.query('UPDATE role SET role_id =? WHERE CONCAT (first_name, " ", last_name)=? ') [answer.updateEmployee, answer.employeeNewRole]
+    
+    })
+   }
  });
 
 
 
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
